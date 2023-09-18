094afea70c4dd6e832932cd17a4a361a6d0c58dc
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-18 14:51:40 +0800

    feat: improve UI of console log

--- src/runtime/plugins/axe.client.ts
+++ src/runtime/plugins/axe.client.ts
@@ -7,11 +7,36 @@ export default defineNuxtPlugin((nuxtApp) => {
 
   // TODO: support introspecting individual components when they render
   async function run() {
-    const { violations } = await axe.run(document, runOptions)
-    for (const result of violations) {
+    const result = await axe.run(document, {
+      elementRef: true,
+      ...runOptions
+    })
+    for (const violation of result.violations) {
       // TODO: support different backends (console logging, devtools, etc.)
-      console.log(result)
+      logViolation(violation)
     }
+  }
+
+  const warned = new Set<string>()
+  const colorMap = {
+    minor: '#f7e167',
+    moderate: '#cf863e',
+    serious: '#f25c54',
+    critical: '#720026'
+  }
+  function logViolation(violation: axe.Result) {
+    const id = violation.id + ':' + violation.impact + ':' + violation.nodes.map(i => i.target).join(',')
+    if (warned.has(id))
+      return
+    warned.add(id)
+    const elements = violation.nodes.filter(i => !i.target?.includes('html') && i.element).map(i => i.element)
+    console[violation.impact === 'critical' ? 'error' : 'warn'](
+      `%ca11y%c ${violation.help}\n  ${violation.helpUrl}\n`,
+      `color: white; border-radius: 3px; padding: 2px 3px; font-size: 0.8em; background: ${colorMap[violation.impact || 'moderate']};`,
+      '',
+      ...elements,
+    )
+    // console.log(violation)
   }
 
   // TODO: trigger on nuxt hook or composable?
