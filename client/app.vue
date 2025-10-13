9c9c441dc2b58dec87e11906ba907d3424bf8f4c
timdamenc <t.damen@clappform.com>
2025-10-13 16:05:12 +0200

    feat: devtools client

---
+++ client/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>
+      Nuxt a11y
+    </h1>
+  </div>
+</template>
+
+<script setup>
+</script>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,45 @@
+import { resolve } from 'node:path'
+import { startSubprocess } from '@nuxt/devtools-kit'
+import { defineNuxtModule } from '@nuxt/kit'
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/a11y'],
+  modules: [
+    '@nuxt/a11y',
+    /**
+     * Start a sub Nuxt Server for developing the client
+     *
+     * The terminal output can be found in the Terminals tab of the devtools.
+     */
+    defineNuxtModule({
+      setup(_, nuxt) {
+        if (!nuxt.options.dev) return
+
+        const subprocess = startSubprocess(
+          {
+            command: 'npx',
+            args: ['nuxi', 'dev', '--port', '3030'],
+            cwd: resolve(__dirname, '../client'),
+          },
+          {
+            id: 'nuxt-a11y:client',
+            name: 'Nuxt a11y Client Dev',
+          },
+        )
+        subprocess.getProcess().stdout?.on('data', (data) => {
+          console.log(` sub: ${data.toString()}`)
+        })
+
+        process.on('exit', () => {
+          subprocess.terminate()
+        })
+
+        // process.getProcess().stdout?.pipe(process.stdout)
+        // process.getProcess().stderr?.pipe(process.stderr)
+      },
+    }),
+  ],
+
   devtools: { enabled: true },
   compatibilityDate: '2024-08-21',
   a11y: {},
